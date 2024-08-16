932a9fe4952803dc6648ccc0e30c4326cd7beb5f
Sarita200 <saritasadgir1@gmail.com>
2024-08-16 20:57:04 +0530

    login and register API created

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
 dotenv.config()
+import User from './models/user.js'
+import Fitness from './models/Fitness.js'
+
 const app = express()
 app.use(express.json());
 app.use(cors());
@@ -25,8 +28,60 @@ app.get('/',(req,res)=>{
     })
 })
 
+app.post('/register',async(req,res) =>{
+    const {fullName,email,mobileNo,dob,password} = req.body;
 
-const PORT = process.env.PORT || 6000
+    const user = new User({
+        fullName:fullName,
+        email:email,
+        mobileNo:mobileNo,
+        dob: new Date(dob),
+        password:password
+    })
+    
+
+    try{
+        const savedUser = await user.save();
+        res.json({
+            message:"User Registered Successfully",
+            success:true,
+            user:savedUser
+            })
+    }
+    catch(e){
+        res.json({
+            message:e.message,
+            success:false,
+            data:null
+        })
+    }
+})
+
+app.post('/login',async(req,res) =>{
+    const {email,password} = req.body;
+
+    const user = await User.findOne({
+        email:email,
+        password:password
+    })
+
+    if(!user){
+        return res.json({
+        success:false,
+        message:"Invalid User",
+        data:null
+        })
+    }
+    else{
+        return res.json({
+            success: true,
+            message:"User Loged in successfully",
+            data:user
+        })
+    }
+})
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT , ()=>{
     console.log(`server is running on port ${PORT}`)
 })
--- server/models/Fitness.js
+++ server/models/Fitness.js
@@ -1,15 +1,38 @@
 import { Schema,model } from "mongoose";
 
 const fitnessSchema = new Schema({
-    time :{
+    duration :{
         type :String,
         required:true
     },
     exercise :{
         type:String,
-        required:true
+        default: "others"
     },
-
+    date: {
+        type: Date,
+        required: true
+    },
+    weight:{
+        type:String,
+        default:0,
+    },
+    reps:{
+        type:String,
+        default:0
+    },
+    yoga:{
+        type: String,
+        default: "others"
+    },
+    bodyweight:{
+        type:String,
+        default:0,
+    },
+    distance:{
+        type: String,
+        default:true
+    }
 },{
 
 },
